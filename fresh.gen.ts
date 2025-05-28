411ab8ca109c9686fb3e8bd46f9dffef0555fbbb
Sean Buckle <seanlbuckle@gmail.com>
2025-05-28 01:55:30 +0100

    Add HomeIsland import to fresh.gen.ts manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -9,6 +9,7 @@ import * as $api_projects from "./routes/api/projects.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
 import * as $Experience from "./islands/Experience.tsx";
+import * as $HomeIsland from "./islands/HomeIsland.tsx";
 import * as $Projects from "./islands/Projects.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
@@ -23,6 +24,7 @@ const manifest = {
   },
   islands: {
     "./islands/Experience.tsx": $Experience,
+    "./islands/HomeIsland.tsx": $HomeIsland,
     "./islands/Projects.tsx": $Projects,
   },
   baseUrl: import.meta.url,
