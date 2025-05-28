87e07fbad195c9a35f85c9f04dcfe6dec4dc1c40
Sean Buckle <seanlbuckle@gmail.com>
2025-05-28 03:23:36 +0100

    Enhance portfolio structure with new components and context management (#9)

--- islands/HomeIsland.tsx
+++ islands/HomeIsland.tsx
@@ -1,21 +1,24 @@
-import { ContentProvider } from "../context/ContentContext.tsx";
 import { Skills } from "../components/Skills.tsx";
 import { Experience } from "./Experience.tsx";
 import { Projects } from "./Projects.tsx";
 
+type Content = {
+  skills: [];
+  experience: [];
+};
+
 type HomeIslandProps = {
-  content: any; // Replace 'any' with your content type if available
+  content: Content;
 };
 
 export default function HomeIsland({ content }: HomeIslandProps) {
-
   return (
-    <ContentProvider content={content}>
+    <main>
       <h1>Welcome to My Portfolio</h1>
       <p>This is a simple portfolio page.</p>
-      <Skills />
-      <Experience experience={content.experience}/>
+      <Skills skills={content.skills} />
+      <Experience experience={content.experience} />
       <Projects />
-    </ContentProvider>
+    </main>
   );
 }
