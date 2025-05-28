74b808a9dd105e15d8789b32d3b5a36d79f15ed6
Sean Buckle <seanlbuckle@gmail.com>
2025-05-28 02:06:36 +0100

    Add HomeIsland component to render portfolio sections with content provider

---
+++ islands/HomeIsland.tsx
@@ -0,0 +1,21 @@
+import { ContentProvider } from "../context/ContentContext.tsx";
+import { Skills } from "../components/Skills.tsx";
+import { Experience } from "./Experience.tsx";
+import { Projects } from "./Projects.tsx";
+
+type HomeIslandProps = {
+  content: any; // Replace 'any' with your content type if available
+};
+
+export default function HomeIsland({ content }: HomeIslandProps) {
+
+  return (
+    <ContentProvider content={content}>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a simple portfolio page.</p>
+      <Skills />
+      <Experience />
+      <Projects />
+    </ContentProvider>
+  );
+}
