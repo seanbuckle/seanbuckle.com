5a1f16e7637b64258be5b49e3be9edafe460030f
Sean Buckle <seanlbuckle@gmail.com>
2025-06-03 13:44:54 +0100

    Refactor Skills component for improved structure and readability

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,31 +6,27 @@ type Skill = {
 
 export function Skills({ skills }: { skills: Skill[] }) {
   return (
-    <section class="skills">
+    <>
+    <section class="skills-wrapper">
       <h2>Skills</h2>
-      {skills.map((skill: Skill, i: number) => (
-        <a
-          key={i}
-          href={skill.link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`skill__link--${
-            skill.skill.toLowerCase().replace(/[ .]/g, "")
-          }`}
-        >
-          <span dangerouslySetInnerHTML={{ __html: skill?.image }} />
-          {skill.skill}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="1.5rem"
-            viewBox="0 -960 960 960"
-            width="1.5rem"
-            fill="currentcolor"
+      <section class="skills">
+        {skills.map((skill: Skill, i: number) => (
+          <a
+            key={i}
+            href={skill.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`skills__link skills__link--${
+              skill.skill.toLowerCase().replace(/[ .]/g, "")
+            }`}
           >
-            <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
-          </svg>
-        </a>
-      ))}
+            <span dangerouslySetInnerHTML={{ __html: skill?.image }} />
+            {skill.skill}
+          </a>
+        ))}
+      </section>
     </section>
+      
+    </>
   );
 }
